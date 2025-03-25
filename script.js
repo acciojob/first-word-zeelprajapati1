4665f79fe47c104de1447657f8e20bad05e5d99d
zeelprajapati1 <zeelprajapati9876@gmail.com>
2025-03-26 02:55:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 function firstWord(s) {
-  // your code here
+    // Trim leading spaces and find the first space
+    let trimmed = s.trim();
+    let index = trimmed.indexOf(" ");
+    
+    // If no space is found, return the entire string
+    return index === -1 ? trimmed : trimmed.slice(0, index);
 }
 
 // Do not change the code below
-
 const s = prompt("Enter String:");
 alert(firstWord(s));
